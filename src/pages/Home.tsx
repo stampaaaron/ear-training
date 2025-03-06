f7695571c4814baed28de87fbb51ccf54b20de33
stampaaaaron <aaron@stampa.ch>
2025-03-06 16:50:28 +0000

    fix: button text wrapping

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Title } from '@mantine/core';
+import { Button, Stack, Title, Text } from '@mantine/core';
 import { basicTensions } from '../notes';
 import { Link } from 'react-router';
 import { Shell } from '../layout/Shell';
@@ -7,7 +7,7 @@ const chordSets = [
   {
     label: 'Basic Tensions',
     description:
-      'Maj7, Maj6, Dom7, Dom7sus4, Min7, Min6, Min7b5 and MinMaj7 with all tensions',
+      'maj7, maj6, dom7, dom7sus4, min7, min6, min7(b5) and min(maj7) with all tensions',
     chords: basicTensions,
   },
 ];
@@ -17,10 +17,19 @@ export function Home() {
     <Shell title="Choose your chord set">
       <Stack>
         {chordSets.map((chordSet) => (
-          <Button variant="outline" component={Link} to="/quiz" h="auto" p="lg">
-            <Stack gap="xs">
-              <Title order={3}>{chordSet.label}</Title>
-              {chordSet.description}
+          <Button
+            variant="outline"
+            component={Link}
+            to="/quiz"
+            h="auto"
+            p="lg"
+            maw="100%"
+          >
+            <Stack gap="xs" w="100%">
+              <Title order={3} w="100%">
+                {chordSet.label}
+              </Title>
+              <Text style={{ whiteSpace: 'wrap' }}>{chordSet.description}</Text>
             </Stack>
           </Button>
         ))}
