f872a65582058ddf6b4a73b2c2bef661ae4d3831
stampaaaaron <aaron@stampa.ch>
2025-03-13 15:26:56 +0000

    feat: replace settings fields with sliders

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { NumberInput, Stack } from '@mantine/core';
+import { Slider, Stack, Text } from '@mantine/core';
 import { Shell } from '../layout/Shell';
 import { useForm } from '@mantine/form';
 import { useStore } from '@nanostores/react';
@@ -22,20 +22,20 @@ export function Settings() {
         }}
       >
         <Stack>
-          <NumberInput
+          <Text>Note to note delay: {form.getValues().noteToNoteDelay}s</Text>
+          <Slider
             {...form.getInputProps('noteToNoteDelay')}
-            label="Note to note delay"
-            rightSection={'Seconds'}
-            rightSectionWidth={100}
+            label={(value) => `${value} Seconds`}
             max={5}
             step={0.1}
             min={0.5}
           />
-          <NumberInput
+          <Text>
+            How long the notes should ring out: {form.getValues().releaseDelay}s
+          </Text>
+          <Slider
             {...form.getInputProps('releaseDelay')}
-            label="How long the notes should ring out"
-            rightSection={'Seconds'}
-            rightSectionWidth={100}
+            label={(value) => `${value} Seconds`}
             max={10}
             step={0.5}
             min={1}
