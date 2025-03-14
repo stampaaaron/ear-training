6db04916c1a142725f8ed599d5afb23ed1e770ba
stampaaaaron <aaron@stampa.ch>
2025-03-14 07:55:12 +0000

    feat: introduce slider marks

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -4,6 +4,7 @@ import { useForm } from '@mantine/form';
 import { useStore } from '@nanostores/react';
 import { $settings, defaultSettings } from '../store/settings';
 import { Frequency } from 'tone';
+import { arrayRange } from '../utils';
 
 type SettingsForm = NonNullable<typeof $settings.value>;
 
@@ -21,6 +22,7 @@ export function Settings() {
       rightSection={
         <Button
           variant="subtle"
+          p="xs"
           onClick={() => {
             $settings.set(defaultSettings);
             form.setValues(defaultSettings);
@@ -42,9 +44,13 @@ export function Settings() {
             <Slider
               {...form.getInputProps('noteToNoteDelay')}
               label={(value) => `${value} Seconds`}
+              min={0.1}
               max={5}
               step={0.1}
-              min={0.1}
+              marks={arrayRange(0, 5).map((value) => ({
+                label: `${value}s`,
+                value: value,
+              }))}
             />
           </Stack>
           <Stack gap="xs">
@@ -55,14 +61,18 @@ export function Settings() {
             <Slider
               {...form.getInputProps('releaseDelay')}
               label={(value) => `${value} Seconds`}
+              min={1}
               max={10}
               step={0.5}
-              min={1}
+              marks={arrayRange(1, 10).map((value) => ({
+                label: `${value}s`,
+                value: value,
+              }))}
             />
           </Stack>
           <Stack gap="xs">
             <Text>
-              Note range (
+              Start note range (
               {Frequency(form.getValues().startNoteRange[0], 'midi').toNote()} -{' '}
               {Frequency(form.getValues().startNoteRange[1], 'midi').toNote()})
             </Text>
@@ -71,6 +81,10 @@ export function Settings() {
               minRange={1}
               min={36} // C2
               max={84} // C6
+              marks={arrayRange(36, 84, 12).map((value) => ({
+                label: Frequency(value, 'midi').toNote(),
+                value: value,
+              }))}
               label={(value) => Frequency(value, 'midi').toNote()}
               step={1}
             />
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
 import { Chord } from './notes';
 
+export const arrayRange = (start: number, stop: number, step: number = 1) =>
+  Array.from(
+    { length: (stop - start) / step + 1 },
+    (_, index) => start + index * step
+  );
 function getRandomFromArray<T>(items: ArrayLike<T>): T {
   return items[Math.floor(Math.random() * items.length)];
 }
